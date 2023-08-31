91f5e3b7c40a9e7720c666a848a25be1c672e988
erixweb <erikmorenodev@gmail.com>
2023-08-31 16:17:49 +0200

    Initial commit

---
+++ src/components/book-link.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Book, Library } from "../types"
+
+	export let book: Library
+</script>
+
+<a href={book.book.ISBN} class="w-full max-w-[400px] bg-zinc-700 rounded-[15px]">
+	<img src={book.book.cover} alt="" class="w-full h-[400px] object-fill rounded-t-[15px]" />
+	<div class="p-[1em]">
+		<h2>
+			{book.book.title}
+		</h2>
+		<p>
+			{book.book.synopsis}
+		</p>
+	</div>
+</a>
---
+++ src/components/books-list.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import BookLink from "./book-link.svelte"
+
+    export let books: any
+
+    let library: any = books["library"]
+</script>
+
+<div class="flex flex-wrap mt-[20px] gap-[20px] p-[20px]">
+    {#each library as book}
+        <BookLink 
+            {book}
+        />
+    {/each}
+</div>  
