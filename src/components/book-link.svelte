dbc4d9af2d7d593c772534ea199b21ae51627c48
erixweb <erikmorenodev@gmail.com>
2023-09-01 16:28:03 +0200

    Change title colors

--- src/components/book-link.svelte
+++ src/components/book-link.svelte
@@ -4,15 +4,16 @@
 	export let book: Library
 </script>
 
-<a href={`libro/${book.book.href}`} class="w-full max-w-[350px] bg-zinc-700 rounded-[15px]">
+<a href={`libro/${book.book.href}`} class="w-full max-w-[350px] bg-zinc-700 rounded-[15px] h-full mt-[0px]">
 	<img
-		src={book.book.cover}
+		data-src={book.book.cover}
+		loading="lazy"
 		alt=""
 		class="w-full h-[350px] object-fill rounded-t-[15px]"
 		style={`view-transition-name: book-${book.book.ISBN}`}
 	/>
 	<div class="p-[1em]">
-		<h2>
+		<h2 class="text-sky-400 font-bold text-[20px] mb-[15px]">
 			{book.book.title}
 		</h2>
 		<p>
--- src/components/book-selector.svelte
+++ src/components/book-selector.svelte
@@ -5,20 +5,53 @@
 </script>
 
 <div class="flex flex-wrap p-[20px]">
-	<div class="ml-[0px]">
+	<div class="ml-[0px] text-center">
 		<img
-			src={book.book.cover}
+			data-src={book.book.cover}
+			loading="lazy"
 			alt=""
 			style={`view-transition-name: book-${book.book.ISBN}`}
 			class="w-[400px] h-[500px] max-w-[100%]"
 		/>
 	</div>
-	<div class="ml-[0px] mt-[0px]">
-		<h1 class="text-[40px] font-bold">
+	<div class="ml-[0px] mt-[0px] p-[16px]">
+		<h1 class="text-[40px] font-bold text-sky-500 mb-[12px]">
 			{book.book.title}
 		</h1>
-        <p>
-            {book.book.synopsis}
-        </p>
+		<h2 class="text-[25px] mb-[20px]">
+			{book.book.author.name}
+		</h2>
+		<p>
+			<strong>
+				{book.book.year}
+			</strong>
+		</p>
+		<p class="mt-[20px]">
+			Libro de
+			<strong>
+				{book.book.genre}
+			</strong>
+		</p>
+		<p>
+			<strong>
+				{book.book.pages}
+			</strong>
+			páginas
+		</p>
+
+		<h2 class="text-[25px] font-bold mt-[20px]">Sinopsis</h2>
+		<p class="max-w-[450px]">
+			{book.book.synopsis}
+		</p>
+		<div>
+			<h2 class="text-[22px] font-bold my-[20px]">Del mismo autor</h2>
+			<ul>
+				{#each book.book.author.otherBooks as otherBook}
+					<li class="mb-[5px]">
+						{otherBook}
+					</li>
+				{/each}
+			</ul>
+		</div>
 	</div>
 </div>
