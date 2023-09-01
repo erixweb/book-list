be9236d8105db7c36e84cefdee7e5cdeb8053f8e
erixweb <erikmorenodev@gmail.com>
2023-09-01 19:54:40 +0200

    Work on saving books

---
+++ src/components/button-add.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let id: any
+
+    let addedBooks: any
+    let currentBook: any
+
+    if (typeof window !== "undefined") {
+        addedBooks = JSON.parse(localStorage.getItem("books-list")!)
+    }
+
+    function addBook () {
+        if (addedBooks) {
+            currentBook = [addedBooks].find((element: any) => element.id === id)
+        }
+        if (!currentBook) {
+            localStorage.setItem("books-list", JSON.stringify([JSON.stringify(addedBooks) + { id }]))
+        }
+    }
+    </script>
+<button on:click={addBook}> Añadir </button>
