27cc745650fa1e40f1ccea3a3542d65bacb5dde6
erixweb <erikmorenodev@gmail.com>
2023-09-04 14:15:57 +0200

    Add filter urls

--- src/components/books-list.svelte
+++ src/components/books-list.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
+	import type { Library } from "../types"
 	import BookLink from "./book-link.svelte"
 
 	export let books: any
+	export let url: URL
+
 
 	let library: any = books["library"]
+	let booksFilter: Library[] = library
+
+	const filter = {
+		name: url.searchParams.get("name")! || "",
+		minpages: url.searchParams.get("minpages") || "",
+		maxpages: url.searchParams.get("maxpages") || ""
+	}
+	
+	if (filter.name) {
+		booksFilter = booksFilter.filter((book: any) => book.book.title.toLocaleLowerCase().includes(filter.name.toLowerCase()))
+	}
+	if (filter.minpages) {
+		booksFilter = booksFilter.filter((book: Library) => book.book.pages >= parseInt(filter.minpages))
+	}
+	if (filter.maxpages) {
+		booksFilter = booksFilter.filter((book: Library) => book.book.pages <= parseInt(filter.maxpages))
+	}
+
+
 </script>
 
 <div class="flex flex-wrap mt-[20px] gap-[20px] p-[20px] w-full">
-	{#each library as book}
+	{#each booksFilter as book}
 		<BookLink 
 			{book}
 			lazy={true}
